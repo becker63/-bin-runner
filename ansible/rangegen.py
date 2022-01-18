6d45e9e184e0ad63470707c3636c84d08f529c81
Becker63 <johnsontaylor6320@gmail.com>
2022-01-17 19:31:13 -0500

    Update rangegen.py

--- ansible/rangegen.py
+++ ansible/rangegen.py
@@ -1,59 +1,40 @@
 #!/usr/bin/python
 #range = input("enter address range: ")
+import json
 from netaddr import IPNetwork
-from collections import ChainMap
 import os
 import nmap
-import collections
-import flatdict
+
+range = []
 
 if os.path.exists("hosts"):
-  os.remove("hosts")
+  print("host exists")
 else:
-  print("file doesnt exist")
+  print("")
+  print("")
+  input = input("enter range of nao subnet: ")
+  print("")
+  print("")
+  nmScan = nmap.PortScanner()
 
-ips = []
-chars = ["[sites]"]
+  nmScan.scan(input, '22')
 
-file = open("hosts", "w")
+  for host in nmScan.all_hosts():
+    if nmScan[host].has_tcp(22) == True:
+        range.append(host)
 
-nm = nmap.PortScanner()
+  ips = []
+  chars = ["[sites]"]
 
-for ip in IPNetwork("192.168.1.0/23"):
+  file = open("hosts", "w")
+
+  nm = nmap.PortScanner()
+
+  for ip in range:
         ips.append(ip)
         writeable = str(ip)
         chars.append(writeable)
 
-out = "\n".join(chars)
-file.write(out)
-file.close
-
-connectrange = ['127.0.0.1','localhost']
-
-def get_values(data):
-    values = []
-    for k, v in data.items():
-        if isinstance(v, dict):
-            values.extend(get_values(v))
-        else:
-            values.append(v)
-    return values
-
-nma = nmap.PortScannerAsync()
-def callback_result(host, scan_result):
-    print('------------------')
-    #print(host,scan_result)
-    a = get_values(scan_result)
-    if('up') in str(a):
-      print(host + " : up")
-      x = str(host)
-      connectrange.append(x)
-      return connectrange
-    else:
-      print("down")
-
-nma.scan('192.168.0.102-104','22', callback=callback_result)
-while nma.still_scanning():
-    nma.wait(1)
-
-print(connectrange)
+  out = "\n".join(chars)
+  file.write(out)
+  file.close
